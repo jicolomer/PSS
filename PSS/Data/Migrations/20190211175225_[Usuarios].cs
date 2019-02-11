e7e7adce3906feb7791a8c3c9ce8d3b59b9992ff
Nacho <jicolomer@cadam.es>
2019-02-11 18:53:21 +0100

    BBDD

---
+++ PSS/Data/Migrations/20190211175225_[Usuarios].cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PSS.Data.Migrations
+{
+    public partial class Usuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
