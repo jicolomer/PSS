f60600c89b08f453c76b8fd5a29a6fe059fd872a
Nacho <jicolomer@cadam.es>
2019-02-11 14:39:39 +0100

    Inicio

--- PSS/Controllers/HomeController.cs
+++ PSS/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PSS.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace PSS.Controllers
 {
@@ -15,6 +16,7 @@ namespace PSS.Controllers
             return View();
         }
 
+        //[Authorize(Roles = "Administrador")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
--- PSS/Data/Migrations/20171002171954_[Migracion-inicial].Designer.cs
+++ PSS/Data/Migrations/20171002171954_[Migracion-inicial].Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PSS.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171002171954_[Migracion-inicial]")]
+    partial class Migracioninicial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
