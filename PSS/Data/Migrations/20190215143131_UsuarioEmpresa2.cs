29eb3275cf883feffaa7aa6f4713b4e98c477eea
Nacho <jicolomer@cadam.es>
2019-02-15 15:33:30 +0100

    Falta la migracion

---
+++ PSS/Data/Migrations/20190215143131_UsuarioEmpresa2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace PSS.Data.Migrations
+{
+    public partial class UsuarioEmpresa2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
