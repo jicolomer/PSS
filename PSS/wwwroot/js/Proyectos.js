eacc46da5ac76a8ee81864622309d5239433ceb1
Jose Ignacio <jicolomer@cadam.es>
2019-03-04 12:52:06 +0100

    ProvinciasCasi

--- PSS/wwwroot/js/Proyectos.js
+++ PSS/wwwroot/js/Proyectos.js
@@ -1,7 +1,9 @@
 ﻿var cont = 0;
 var cont2 = 0;
+var cont3 = 0;
 
 function CompruebaProyectos() {
+    alert("pasa");
     $('input[name=TipoEstudio]')[0].value = document.getElementById('SelectTipoEstudio').value;
     $('input[name=ProvinciaObra]')[0].value = document.getElementById('SelectProvinciaObra').value;
 
@@ -46,4 +48,47 @@ function getProvincias(action) {
             }
         }
     });
+}
+
+function getProvinciasID(action,ID) {
+
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: {},
+        success: function (response) {
+            if (cont3 == 0) {
+                for (var i = 0; i < response.length; i++) {
+                    if (response[i].value!=ID) document.getElementById('SelectProvinciaObra').options[i] = new Option(response[i].text, response[i].value,false,false);
+                    else document.getElementById('SelectProvinciaObra').options[i] = new Option(response[i].text, response[i].value, false, true);
+                }
+                cont3 = 1;
+            }
+        }
+    });
+}
+function getTipoEstudiosID(action,ID) {
+
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: {},
+        success: function (response) {
+            if (cont == 0) {
+                for (var i = 0; i < response.length; i++) {
+                    if (ID != response[i].value) document.getElementById('SelectTipoEstudio').options[i] = new Option(response[i].text, response[i].value,false,false);
+                    else  document.getElementById('SelectTipoEstudio').options[i] = new Option(response[i].text, response[i].value, false, true);
+                }
+                cont = 1;
+            }
+        }
+    });
+}
+
+function CargaProyectos() {
+    getProvinciasID('../../Provincias/GetProvincias', $('input[name=ProvinciaObra]')[0].value);
+    getTipoEstudiosID('../GetTiposEstudio', $('input[name=TipoEstudio]')[0].value);
+
+    
+
 }
